1868c5d31cbf67f7c40720583d7f722950a3b628
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-21 22:42:22 -0300

    feat: add UserModel

---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+class UserModel {
+  final String name;
+  final int score;
+  final String photoUrl;
+
+  UserModel({
+    required this.name,
+    this.score = 0,
+    required this.photoUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'score': score,
+      'photoUrl': photoUrl,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'],
+      photoUrl: map['photoUrl'],
+      score: map['score'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
+}
